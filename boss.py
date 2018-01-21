ea84dd13228c6917e8b0045cc6701ab6041310a1
makalabay <makala.bay@gmail.com>
2018-01-21 10:17:01 +0000

    oops

--- boss.py
+++ boss.py
@@ -1,4 +1,4 @@
-# import emotion_api
+import emotion_api
 import analyze
 import time
 import getpass
@@ -29,35 +29,36 @@ if __name__ == '__main__':
     # elif platform == "win32" or platform == "cygwin":
     # Windows...
 
-    # logfile = open(logfileLocation,"r")
-    # loglines = follow(logfile)
-    # for line in loglines:
-    #     print("reading 1")
-    #     if " -------- Loading map 'map01_ch1.map' ---------" in line:
-    #         print("Recognized: Entering Map 1")
-    #         break
-    #     elif " -------- Loading complete ---------" in line:
-    #         print("Recognized: Loaded Map 1")
-    #         break
-    # logfile.close()
-    # # records images until map03 begins to load
-    # #     counter = 1
-    # logfile = open(logfileLocation,"r")
-    # loglines = follow(logfile)
-    # for line in loglines:
-    #     print("reading 2")
-    #     if "Trigger: Leaving Map 1" in line:
-    #         print("Recognized: Leaving Map 1")
-    #         break
-    #         #     	fileName = "p_" + counter
-    #         #     	emotion_simple(fileName)
-    #         #     	time.sleep(0.5)
-    #         #     	counter += 1
-    # logfile.close()
+    logfile = open(logfileLocation,"r")
+    loglines = follow(logfile)
+    for line in loglines:
+        print("reading 1")
+        if " -------- Loading map 'map01_ch1.map' ---------" in line:
+            print("Recognized: Entering Map 1")
+            break
+        elif " -------- Loading complete ---------" in line:
+            print("Recognized: Loaded Map 1")
+            break
+    logfile.close()
+    # records images until map03 begins to load
+    counter = 1
+    logfile = open(logfileLocation,"r")
+    loglines = follow(logfile)
+    for line in loglines:
+        print("reading 2")
+        if "Trigger: Leaving Map 1" in line:
+            print("Recognized: Leaving Map 1")
+            break
+        else:
+        	fileName = "p_" + counter
+        	emotion_api.emotion_simple(fileName)
+        	time.sleep(0.5)
+        	counter += 1
+    logfile.close()
 
     # gets jumps in fear from each scare
-    # fearNums = analyze("p_", counter)
-    fearNums = analyze.genFearScores("p", 4)
+    fearNums = analyze.genFearScores("p_", counter)
+    # fearNums = analyze.genFearScores("p", 4)
     fearNums = [int(x * 100) for x in fearNums]
     fearLevels = {'jumpscare':fearNums[0], 'suspense':fearNums[1], 'insanity':fearNums[2], 'monsters':fearNums[3]}
     # fearScale = OrderedDict(sorted(fearLevels.items(), key=itemgetter(1), reverse=True)) # sorts scares by most terrifying to least
@@ -65,11 +66,9 @@ if __name__ == '__main__':
 
     # appends void getFearLevels() to map03_ch1.hps
     getFearLevels = "void getFearLevels(){"
-    rank = 1
     # creates void getFearLevels()
     for scare, fearLevel in fearLevels.items():
-        getFearLevels = getFearLevels + "SetGlobalVarInt(\"" + scare + "\"," + str(rank) + ");"
-        rank += 1
+        getFearLevels = getFearLevels + "SetGlobalVarInt(\"" + scare + "\"," + fearLevel + ");"
     getFearLevels += "}"
     print(getFearLevels)
 
